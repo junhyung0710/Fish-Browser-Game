9d69c985c650eb8c07cac54af2019da896485d92
junhyung0710 <junhyung0710@gmail.com>
2022-12-23 17:57:23 -0800

    forgot to save

--- fish-frontend/src/Components/UI.js
+++ fish-frontend/src/Components/UI.js
@@ -2,13 +2,11 @@ import '../css/UI.css'
 import ButtonDisplay from './UI_Components/ButtonDisplay'
 function UI() {
     return(
-        <div className = 'container'>
-            <div className = 'toggle-switch'>
-                <label>
-                    <input type = 'checkbox'/>
-                    <span className = 'slider'></span>
-                </label>
-            </div>
+        <div className = 'toggle-switch'>
+            <label>
+                <input type = 'checkbox'/>
+                <span className = 'slider'></span>
+            </label>
         </div>
         
     )
